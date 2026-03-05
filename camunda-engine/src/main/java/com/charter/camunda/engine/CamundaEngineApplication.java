4c868bd53b09ff3de25b28e79631a8cfea11f2a2
Kiran Kumar H.M <kirankumarhm2004@gmail.com>
2026-03-05 17:05:53 +0530

    Create CamundaEngineApplication class for Spring Boot

---
+++ camunda-engine/src/main/java/com/charter/camunda/engine/CamundaEngineApplication.java
@@ -0,0 +1,12 @@
+package com.charter.camunda.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaEngineApplication.class, args);
+    }
+}
