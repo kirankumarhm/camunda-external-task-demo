be409b59ca7dace43de347e97959081e7423b1f4
Kiran Kumar H.M <kirankumarhm2004@gmail.com>
2026-03-06 14:52:38 +0530

    Create PaymentWorkerApplication class

---
+++ payment-worker/src/main/java/com/charter/camunda/worker/payment/PaymentWorkerApplication.java
@@ -0,0 +1,12 @@
+package com.charter.camunda.worker.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentWorkerApplication.class, args);
+    }
+}
